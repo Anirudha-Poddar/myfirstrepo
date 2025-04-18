4106779eb9561adca4a1da5f9cfc846945e92876
Anirudha-Poddar <poddar.anirudha@gmail.com>
2025-04-18 14:05:09 +0530

    Second Commit

--- first_code.py
+++ first_code.py
@@ -1,9 +1,10 @@
 # Define two variables
 a = 56
 b = 20
+c = 10
 
 # Add the variables
-result = a + b
+result = a + b + c
 
 # Print the result
-print("The sum of a and b is:", result)
+print("The sum of a , b and c is:", result)
